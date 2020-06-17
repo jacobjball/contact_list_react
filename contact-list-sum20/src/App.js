0eb8982079881c2d71846aa156e869910e576e4f
Jake Ball <jacobjball18@gmail.com>
2020-06-16 19:41:04 -0600

    contacts/contact component

--- contact-list-sum20/src/App.js
+++ contact-list-sum20/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+import Contacts from './components/contacts/Contacts';
+class App extends Component {
+  state = { contacts: [
+    { id: 1, firstName: 'John', phone: '123-123-1233'},
+    { id: 2, firstName: 'Sally', phone: '123-333-1233'},
+    { id: 3, firstName: 'Alex', phone: '123-125-1233'},
+  ]}
+  render() {
+    const { contacts } = this.state
+    return(
+      <div>
+        <Header size="huge" color='blue' textAlign='center'>
+          Contact list
+        </Header>
+        <Contacts contacts={contacts} />
+      </div>
+    )
+  }
 }
-
 export default App;
